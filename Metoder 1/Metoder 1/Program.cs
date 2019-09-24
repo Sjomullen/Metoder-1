856394458ec286be9ddf3c09df426924e6354603
Sjomullen <54435499+Sjomullen@users.noreply.github.com>
2019-09-24 13:49:14 +0200

    Gjorde klart Metoder 1

---
+++ Metoder 1/Metoder 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Metoder_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+        }
+          
+        static double CirkelOmkrets(double r)
+        {
+            return (r * 2 * Math.PI);
+        }
+
+        static double CirkelArea(double r)
+        {
+            return (r*r*Math.PI);
+        }
+    }
+}
